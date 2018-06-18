bf7bd96812beda67aab444f0de002229904b6688
Jordan Wilson <jordan.f.wilson@gmail.com>
2018-06-18 16:37:41 +1200

    refactor

--- controllers/register.js
+++ controllers/register.js
@@ -1,69 +1,76 @@
 const handleRegister = async (req, res, db, bcrypt) => {
   const { account, firstname, lastname, email, password } = req.body;
   if (!account || !firstname || !lastname || !email || !password) {
-    return res.status(400).json('incorrect form submission');
+    return res.status(400).json("incorrect form submission");
   }
   try {
-  const acctrx = await db.transaction(trx => {
-      return trx.insert({
-        displayname: account,
-        name: account.toLowerCase(),
-        owner: email,
-        created_at: new Date()
-      })
-      .into('databaselist')
-  })
-  const addRequestsTable = await db.schema.createTable(`${account.toLowerCase()}_requests`,(table) => {
-    table.increments();
-    table.string('firstname');
-    table.string('lastname');
-    table.string('account');
-    table.string('mobile');
-    table.string('home');
-    table.string('twitter');
-    table.string('facebook');
-    table.string('email');
-    table.string('address');
-    table.string('type');
-    table.string('topic');
-    table.string('assign_person');
-    table.string('assign_team');
-    table.string('priority');
-    table.string('details');
-    table.string('attachments');
-    table.string('status');
-    table.string('comments');
-    table.string('user');
-    table.timestamps();
-  })
-  const addUserTable = await db.schema.createTable(`${account.toLowerCase()}_users`,(table) => {
-    table.increments();
-    table.string('firstname');
-    table.string('lastname');
-    table.string('email');
-    table.string('hash');
-    table.string('access');
-    table.string('team');
-  })
-  const usertrx = await db.transaction(trx => {
-    const hash = bcrypt.hashSync(password);
-    return trx.insert({
-      firstname: firstname,
-      lastname: lastname,
-      email: email,
-      hash: hash,
-      access: "admin",
-      team: "Customer Services",
-    })
-    .into(`${account}_users`)
-  })
-  res.status(200).json(account)
+    const acctrx = await db.transaction(trx => {
+      return trx
+        .insert({
+          displayname: account,
+          name: account.toLowerCase(),
+          owner: email,
+          created_at: new Date()
+        })
+        .into("databaselist");
+    });
+    const addRequestsTable = await db.schema.createTable(
+      `${account.toLowerCase()}_requests`,
+      table => {
+        table.increments();
+        table.string("firstname");
+        table.string("lastname");
+        table.string("account");
+        table.string("mobile");
+        table.string("home");
+        table.string("twitter");
+        table.string("facebook");
+        table.string("email");
+        table.string("address");
+        table.string("type");
+        table.string("topic");
+        table.string("assign_person");
+        table.string("assign_team");
+        table.string("priority");
+        table.string("details");
+        table.string("attachments");
+        table.string("status");
+        table.string("comments");
+        table.string("user");
+        table.timestamps();
+      }
+    );
+    const addUserTable = await db.schema.createTable(
+      `${account.toLowerCase()}_users`,
+      table => {
+        table.increments();
+        table.string("firstname");
+        table.string("lastname");
+        table.string("email");
+        table.string("hash");
+        table.string("access");
+        table.string("team");
+      }
+    );
+    const usertrx = await db.transaction(trx => {
+      const hash = bcrypt.hashSync(password);
+      return trx
+        .insert({
+          firstname: firstname,
+          lastname: lastname,
+          email: email,
+          hash: hash,
+          access: "admin",
+          team: "Customer Services"
+        })
+        .into(`${account}_users`);
+    });
+    res.status(200).json(account);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json("unable to register");
   }
-  catch(err) { 
-    console.log(err)
-    res.status(400).json('unable to register')
-  }
-}
+};
 
 module.exports = {
   handleRegister: handleRegister
--- controllers/signin.js
+++ controllers/signin.js
@@ -1,69 +1,79 @@
-const jwt = require('jsonwebtoken');
-const redis = require('redis');
+const jwt = require("jsonwebtoken");
+const redis = require("redis");
 const redisClient = redis.createClient(process.env.REDIS_URI);
 
-const signToken = (id,account) => {
+const signToken = (id, account) => {
   const jwtPayload = { id, account };
-  return jwt.sign(jwtPayload, 'JWT_SECRET_KEY', { expiresIn: '7 days'});
-}
+  return jwt.sign(jwtPayload, "JWT_SECRET_KEY", { expiresIn: "7 days" });
+};
 
 const setToken = (key, value) => Promise.resolve(redisClient.set(key, value));
 
 const createSession = async (user, account) => {
   try {
     const { email, id } = user;
-    const token = signToken(id,account);
-    data = await setToken(token, `${account}_${id}`)
-    return Promise.resolve({ success: 'true', id, token, email, account });
-  } catch(err) {
-    console.log(err)
+    const token = signToken(id, account);
+    data = await setToken(token, `${account}_${id}`);
+    return Promise.resolve({ success: "true", id, token, email, account });
+  } catch (err) {
+    console.log(err);
   }
-}
+};
 
 const handleSignin = async (db, bcrypt, req, res) => {
   const { account, firstname, lastname, email, password } = req.body;
   if (!account || !email || !password) {
-    return Promise.reject('incorrect form submission');
+    return Promise.reject("incorrect form submission");
   }
   try {
-    const data = await db.select('email', 'hash').from(`${account.toLowerCase()}_users`).where('email', '=', email)
+    const data = await db
+      .select("email", "hash")
+      .from(`${account.toLowerCase()}_users`)
+      .where("email", "=", email);
     const isValid = bcrypt.compareSync(password, data[0].hash);
     if (isValid) {
-      const user = await db.select('*').from(`${account.toLowerCase()}_users`).where('email', '=', email)
-      return Promise.resolve({ email: user[0].email, id: user[0].id, account: account })
+      const user = await db
+        .select("*")
+        .from(`${account.toLowerCase()}_users`)
+        .where("email", "=", email);
+      return Promise.resolve({
+        email: user[0].email,
+        id: user[0].id,
+        account: account
+      });
+    } else {
+      return Promise.reject("wrong credentials");
     }
-    else {
-      return Promise.reject('wrong credentials');
-    }
-  } catch(err) {
-    return res.status(400).json('unable to get user');
+  } catch (err) {
+    return res.status(400).json("unable to get user");
   }
-}
+};
 
 const signinAuthentication = async (req, res, db, bcrypt) => {
   const { authorization } = req.headers;
   //Has the person got a token, if so check the token before checking credentials
-  if (authorization) { 
+  if (authorization) {
     redisClient.get(authorization, (err, reply) => {
-      if (reply) { return res.json({reply}) }
+      if (reply) {
+        return res.json({ reply });
+      }
     });
   }
   try {
     //Person has not got a valid token, check their credentials
     const data = await handleSignin(db, bcrypt, req, res);
     //If credentials are valid, create a new session.
-    if (data.id && data.email && req.body.account) { 
+    if (data.id && data.email && req.body.account) {
       const session = await createSession(data, req.body.account);
-      return res.status(200).json(session)
+      return res.status(200).json(session);
     }
-    //Credentials are bad
-    return res.json(Promise.reject(data))
+    return res.json(Promise.reject(data));
   } catch (err) {
-    res.status(400).json(err)
+    res.status(400).json(err);
   }
-}
+};
 
 module.exports = {
   signinAuthentication: signinAuthentication,
   redisClient: redisClient
-}
+};
