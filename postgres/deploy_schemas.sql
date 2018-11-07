9776545c945529416c38308d867b2ccc74e9ca43
Jordan Wilson <jordan.f.wilson@gmail.com>
2018-11-07 18:52:11 +0000

    init deploy

--- postgres/deploy_schemas.sql
+++ postgres/deploy_schemas.sql
@@ -1,10 +1,10 @@
 -- Deploy fresh database tables:
 \i '/docker-entrypoint-initdb.d/tables/databaselist.sql'
+
+-- For testing purposes only. This will add dummy data
+\i '/docker-entrypoint-initdb.d/seed/seed.sql'
 \i '/docker-entrypoint-initdb.d/tables/demo_requests.sql'
 \i '/docker-entrypoint-initdb.d/tables/demo_users.sql'
 \i '/docker-entrypoint-initdb.d/tables/demo_teams.sql'
 \i '/docker-entrypoint-initdb.d/tables/demo_notifications.sql'
 \i '/docker-entrypoint-initdb.d/tables/demo_1_comments.sql'
-
--- For testing purposes only. This file will add dummy data
-\i '/docker-entrypoint-initdb.d/seed/seed.sql'
